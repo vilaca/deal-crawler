5e51d45b2e835802ddc17218e9b31eb61e5bfddc
Vilaca <vilaca@users.noreply.github.com>
2026-04-21 23:22:19 +0100

    Remove brotli/zstd from Accept-Encoding to fix decoding on some platforms

--- utils/http_client.py
+++ utils/http_client.py
@@ -108,7 +108,7 @@ class HttpClient:
                 "text/html,application/xhtml+xml,application/xml;q=0.9," "image/avif,image/webp,image/apng,*/*;q=0.8"
             ),
             "Accept-Language": "pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Encoding": "gzip, deflate",
             "Connection": "keep-alive",
             "Upgrade-Insecure-Requests": "1",
             "Sec-Fetch-Dest": "document",
